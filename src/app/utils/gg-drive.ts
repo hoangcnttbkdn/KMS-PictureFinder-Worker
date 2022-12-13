becbe9bad4de13548199f8078327f5e50a75f7e2
VoVanThanh <thanhvo618@gmail.com>
2022-12-13 21:07:30 +0700

    feat: kms 44 save gg token in database

--- src/app/utils/gg-drive.ts
+++ src/app/utils/gg-drive.ts
@@ -1,21 +1,22 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import { JSONClient } from 'google-auth-library/build/src/auth/googleauth'
 import { google } from 'googleapis'
-import * as fs from 'fs/promises'
-import path from 'path'
+import { TokenRepository } from '../repositories'
 
 class GoogleDriveHelper {
-  private TOKEN_PATH: string
-
+  private tokenRepository: TokenRepository
   constructor() {
-    this.TOKEN_PATH = path.join(process.cwd(), 'gg-token.json')
+    this.tokenRepository = new TokenRepository()
   }
 
   private authorize = async (): Promise<JSONClient> => {
     try {
-      const content = await fs.readFile(this.TOKEN_PATH)
-      const credentials = JSON.parse(content.toString())
-      return google.auth.fromJSON(credentials)
+      const token = await this.tokenRepository.findOne({ where: { id: 1 } })
+      return google.auth.fromJSON({
+        type: token.type,
+        client_id: token.clientId,
+        client_secret: token.clientSecret,
+        refresh_token: token.refreshToken,
+      })
     } catch (err) {
       return null
     }
